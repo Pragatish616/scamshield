e4f967713960bd4df78f43425a57a5d047e2d64c
Pragatish616 <npragatish@gmail.com>
2026-03-05 21:40:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import os
+
+app = FastAPI(title="ScamShield Spam Detector API")
+
+def find_model():
+    if os.path.exists("scamshield/spam_svm_tfidf.joblib"):
+        return "scamshield/spam_svm_tfidf.joblib"
+
+    for root, dirs, files in os.walk("/kaggle/input"):
+        for f in files:
+            if f == "spam_svm_tfidf.joblib":
+                return os.path.join(root, f)
+
+model_path = find_model()
+
+bundle = joblib.load(model_path)
+vectorizer = bundle["vectorizer"]
+model = bundle["model"]
+
+class Message(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"message": "ScamShield API running"}
+
+@app.post("/predict")
+def predict(data: Message):
+    vec = vectorizer.transform([data.message])
+    pred = model.predict(vec)[0]
+    label = "spam" if pred == 1 else "ham"
+    return {"prediction": label}
